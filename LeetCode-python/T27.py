9d692d080dbb08c8718bb540a4416e1b03d97ba1
Dawsen64 <57970377+Dawsen64@users.noreply.github.com>
2021-07-20 14:58:13 +0800

    Create T27.py

---
+++ LeetCode-python/T27.py
@@ -0,0 +1,22 @@
+class Solution:
+    def removeElement(self, nums: list[int], val: int) -> int:
+        count = 0
+        for i in nums:
+            if i == val:
+                count += 1
+        if count == len(nums):
+            return 0
+        for i in range(len(nums) - count, len(nums)):
+            if nums[i] != val:
+                for j in range(0, len(nums) - count):
+                    if nums[j] == val:
+                        nums[j] = nums[i]
+                        break
+
+        print(nums)
+        return len(nums) - count
+
+x = Solution()
+nums = [0,1,2,2,3,0,4,2]
+k = 2
+print(x.removeElement(nums, k))
