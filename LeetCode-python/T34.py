0206d553340885087d15f996b472d55565e024b7
Dawsen <57970377+Dawsen64@users.noreply.github.com>
2021-07-21 17:59:39 +0800

    Update T34.py

--- LeetCode-python/T34.py
+++ LeetCode-python/T34.py
@@ -18,3 +18,33 @@ class Solution:
                 left = mid + 1
         
         return [-1, -1]
+
+    ###二分查找，太妙了
+    class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums) == 0:
+            return [-1, -1]
+        #先找左边界
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if target == nums[mid]:
+                right = mid -1
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        res = left
+        #找右边界
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if target == nums[mid]:
+                left = mid + 1
+            elif target > nums[mid]:
+                left = mid + 1
+            else:
+                right = mid - 1
+        if res <= right:
+            return [res, right]
+        else: return [-1, -1]
