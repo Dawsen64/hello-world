f5696e0ceb9ab7e8cbe4e56f30ebe1926bea6c37
Dawsen64 <57970377+Dawsen64@users.noreply.github.com>
2021-07-21 13:22:32 +0800

    Update T剑指offer52.cpp

--- LeetCode-python/T剑指offer52.cpp
+++ LeetCode-python/T剑指offer52.cpp
@@ -28,3 +28,36 @@ public:
         return nullptr;
     }
 };
+//双指针法
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == nullptr || headB == nullptr)
+            return nullptr;
+        ListNode* pA, *pB;
+        pA = headA;
+        pB = headB;
+        
+        while(pA != nullptr || pB != nullptr)
+        {
+            if(pA == nullptr)
+                pA = headB;
+            if(pB == nullptr)
+                pB = headA;
+            if(pA == pB)
+                return pA;
+
+            pA = pA->next;
+            pB = pB->next;
+        }
+        return nullptr;
+    }
+};
