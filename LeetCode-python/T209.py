8893ab9020412f33dd8da7b4b1c1e1be258f7e44
Dawsen <57970377+Dawsen64@users.noreply.github.com>
2021-07-24 18:11:10 +0800

    Update T209.py

--- LeetCode-python/T209.py
+++ LeetCode-python/T209.py
@@ -21,3 +21,20 @@ class Solution:
         if res < len(nums) + 1:
             return res
         else: return 0
+##官方改良版，我写得太丑了
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left, right = 0, 0
+        res = len(nums) + 1
+        total = 0
+        while right < len(nums):
+            total += nums[right]
+            while total >= target:
+                res = min(res, right - left + 1)
+                total -= nums[left]
+                left += 1
+            right += 1
+
+        if res <= len(nums):
+            return res
+        else: return 0
