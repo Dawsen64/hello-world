bbe823d0a7995e19211877ae6833ff795e7ccf79
Dawsen <57970377+Dawsen64@users.noreply.github.com>
2021-09-08 10:40:14 +0800

    Create T206.cpp

---
+++ LeetCode-python/T206.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+//我自己写的，对于边缘没有处理好
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if (head == nullptr || head->next == nullptr) 
+        {
+            return head;
+        }
+        else if (head->next->next == nullptr)
+        {
+            ListNode* tmp = head->next;
+            head->next->next = head;
+            head->next = nullptr;
+            head = tmp;
+            return head;
+        }
+        ListNode* ptr1 = head->next, *ptr2 = ptr1->next; 
+        head->next = nullptr;
+
+        while (ptr2->next != nullptr)
+        {
+            ptr1->next = head;
+            head = ptr1;
+            ptr1 = ptr2;
+            ptr2 = ptr2->next;
+        }
+        //最后两个节点的时候跳出循环，在循环外反转
+        ptr1->next = head;
+        head = ptr1;
+        ptr1 = ptr2;
+        ptr1->next = head;
+        head = ptr1;
+
+        return head;
+    }
+};
