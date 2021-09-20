79b881da2c57ea195978cfe6dad0086a53f21294
木秋 <57970377+Dawsen64@users.noreply.github.com>
2021-09-20 16:31:06 +0800

    Create T104.cpp

---
+++ LeetCode-python/T104.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+        {
+            return 0;
+        }
+        else
+        {
+            int leftd = maxDepth(root->left);
+            int rightd = maxDepth(root->right);
+            return leftd > rightd ? leftd + 1 : rightd + 1;
+        }
+    }
+};
