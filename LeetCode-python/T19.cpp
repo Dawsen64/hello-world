d6ea88fab35aa14a162e6262e3d225ac6a61661c
木秋 <57970377+Dawsen64@users.noreply.github.com>
2021-11-03 16:44:09 +0800

    对于添加的前置头结点的理解还不够

--- LeetCode-python/T19.cpp
+++ LeetCode-python/T19.cpp
@@ -33,3 +33,37 @@ public:
         }        
     }
 };
+
+//上面的这个版本也是双指针法，但是不简洁
+
+//修改后的双指针法
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode *forehead = new ListNode(0, head);
+        ListNode *first = head, *second = forehead;
+        short i = 1;
+        while (i < n)
+        {
+            first = first->next;
+            i++;
+        }
+        while (first->next != nullptr)
+        {
+            first = first->next;
+            second = second->next;
+        }
+        second->next = second->next->next;
+        return forehead->next;
+    }
+};
