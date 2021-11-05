99c6ac01406d1aa8da7782da1c6f47f61d5d8955
木秋 <57970377+Dawsen64@users.noreply.github.com>
2021-11-05 23:42:16 +0800

    Update T02.07.cpp

--- LeetCode-python/T02.07.cpp
+++ LeetCode-python/T02.07.cpp
@@ -28,3 +28,32 @@ public:
         return nullptr;
     }
 };
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        phead1 = ListNode(-1, headA)
+        phead2 = ListNode(-1, headB)
+        p1 = phead1
+        p2 = phead2
+        flag1, flag2 = False, False
+        while ~flag1 or ~flag2:
+            if p1.next == p2.next:
+                return p1.next
+
+            if p1.next == None:
+                p1 = phead2
+                flag1 = True
+            else:   
+                p1 = p1.next
+
+            if p2.next == None:
+                p2 = phead1
+                flag2 = True
+            else:
+                p2 = p2.next
+        return None
